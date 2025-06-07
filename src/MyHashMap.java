7ec7e72feba6331f9fb3388fc42b2807024cd0c9
barsukova13 <barsukovavitebsk13@gmail.com>
2025-06-07 12:53:17 +0300

    fourth commit

--- src/MyHashMap.java
+++ src/MyHashMap.java
@@ -26,4 +26,80 @@ public class MyHashMap <K, V> {
         hash = hash ^ (hash >>> 16);
         return hash & (table.length -1);
    }
+   public void put(K key, V value){
+        if(size >= LOAD_FACTOR*table.length){
+            resize();
+        }
+        int index = getBucketIndex(key);
+        Entry<K, V> entry = table[index];
+        while (entry != null){
+            if (keysEqual(entry.key, key)){
+                entry.value = value;
+                return;
+            }
+            entry = entry.next;
+        }
+        Entry<K, V> newEntry = new Entry<>(key, value);
+        newEntry.next = table[index];
+        table[index] = newEntry;
+        size++;
+   }
+
+   public V get(K key){
+        int index = getBucketIndex(key);
+        Entry<K, V> entry= table[index];
+
+        while(entry !=null){
+            if (keysEqual(entry.key, key)){
+                return entry.value;
+            }
+            entry = entry.next;
+        }
+        return null;
+   }
+   public V remove(K key){
+        int index = getBucketIndex(key);
+        Entry<K, V> current = table[index];
+        Entry<K, V> previous = null;
+
+        while( current !=null){
+            if(keysEqual(current.key, key)){
+                if(previous == null){
+                    table[index] = current.next;}
+                else {
+                    previous.next = current.next;
+                }
+                size--;
+                return  current.value;
+                }
+            previous = current;
+            current = current.next;
+            }
+        return null;
+        }
+
+        private void resize(){
+        Entry<K, V>[] oldTable = table;
+        table = (Entry <K, V>[]) new Entry[oldTable.length *2];
+        size=0;
+
+        for(Entry<K, V> entry: oldTable){
+            while (entry !=null){
+                put(entry.key, entry.value);
+                entry = entry.next;
+            }
+        }
+   }
+   private boolean keysEqual(K key1, K key2){
+        if (key1==key2) return true;
+        if (key1==null || key2 == null) return false;
+        return key1.equals(key2);
+   }
+
+   public int size(){
+        return size;
+   }
+   public boolean isEmpty(){
+        return size ==0;
+   }
 }
