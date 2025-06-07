a793aa8406302f64edc1b92779c8e9702f185001
barsukova13 <barsukovavitebsk13@gmail.com>
2025-06-07 12:00:38 +0300

    third commit

--- src/MyHashMap.java
+++ src/MyHashMap.java
@@ -1,5 +1,29 @@
-public class MyHashMap {
-    public static void main(String[] args) {
+public class MyHashMap <K, V> {
 
-    }
+   private static class Entry <K, V>{
+       final K key;
+       V value;
+       Entry<K, V> next;
+
+       Entry(K key, V value){
+           this.key = key;
+           this.value = value;
+       }}
+
+   private static final int DEFAULT_CAPACITY = 16;
+   private static final float LOAD_FACTOR = 0.75f;
+   private Entry<K, V>[] table;
+   private int size;
+    @SuppressWarnings("unchecked")
+   public MyHashMap(){
+       table =(Entry<K, V>[]) new Entry[DEFAULT_CAPACITY];
+   }
+   private int getBucketIndex(K key){
+        if (key ==null){
+            return 0;
+        }
+        int hash = key.hashCode();
+        hash = hash ^ (hash >>> 16);
+        return hash & (table.length -1);
+   }
 }
